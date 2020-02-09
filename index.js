e6634bb5d7b184f177fe80fb1eb55f95a6b47dc3
specialblend <cj@specialblend.net>
2020-02-09 04:46:19 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const { compose, concat, head, invertObj, replace } = require('ramda');
+
+const mapAlias = replace(/\*/, '(.*)');
+const mapTarget = compose(concat('<rootDir>'), replace(/\*/, '$1'), head);
+
+const mapAliases = compose(invertObj, map(mapAlias), invertObj);
+const mapTargets = map(mapTarget);
+
+const mapPaths = compose(mapAliases, mapTargets);
+
+module.exports = mapPaths;
